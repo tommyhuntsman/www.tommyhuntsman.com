ce9710f13862803203a73126824663b8e910f24b
Tommy Huntsman <thomas.huntsman@gmail.com>
2017-09-18 19:20:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ $('.your-class').slick({
 
 /* The code below enables the panel toggle */
   $("#portfolio").click(function(){
-      $("#portfoliocontent").slideToggle("slow");
+      $("#portfoliocontent").show("slow");
       $("#webappscontent").slideUp("slow");
       $("#quotescontent").slideUp("slow");
       $("#leaderscontent").slideUp("slow");
