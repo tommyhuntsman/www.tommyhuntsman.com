fd9f7c4ef91c587a6ab26652e1f7916ce36ad812
Tommy Huntsman <thomas.huntsman@gmail.com>
2018-04-23 19:06:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -93,6 +93,12 @@ document.getElementById(id).onclick = function(){}
 
 $(document).ready(function(){
 
+/* Learning JS */
+  function buttonHighlight() {
+    var webApps = document.getElementById("webapps");
+    webApps. classList.toggle("buttonHighlightClass");
+  }
+  
   /* The code below will enable Google Analytics */
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
