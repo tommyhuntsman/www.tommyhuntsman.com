304b54b3815b6902294bcd63018dbaf60a3ac3e9
Tommy Huntsman <thomas.huntsman@gmail.com>
2018-04-24 15:00:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -93,13 +93,13 @@ document.getElementById(id).onclick = function(){}
 
 $(document).ready(function(){
 
-/* Learning JS */
+/* Button Caret 
   function btnClick() {
     var webAppsBtn = document.getElementById("webapps");
     var wbeAppsCaret = document.getElementById("caret");
     webAppsBtn.classList.toggle("btnClick");
     webAppsCaret.classList.toggle("fas fa-caret-down");
-  }
+  } */
   
   /* The code below will enable Google Analytics */
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -113,7 +113,7 @@ $(document).ready(function(){
 /* The code below enables the panel toggle */
   $("#webapps").click(function(){
       $("#webappscontent").slideToggle("slow");
-      //$("#caret").toggleClass("caretDown");
+      $("#caret").toggleClass("fas fa-caret-down");
       $("#portfoliocontent").slideUp("slow");
       $("#quotescontent").slideUp("slow");
       $("#leaderscontent").slideUp("slow");
